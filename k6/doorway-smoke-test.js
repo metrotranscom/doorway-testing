e8fc45c054040ed8555b4b64badd84af7e018fd2
Glenn Brown <gbrown@bayareametro.gov>
2024-02-14 12:45:24 -0800

    feat: taking environment name

--- k6/doorway-smoke-test.js
+++ k6/doorway-smoke-test.js
@@ -1,5 +1,10 @@
+
 import get from './listings/get.js';
 export default function () {
-    const url = 'https://backend.staging.housingbayarea.mtc.ca.gov/listings'
+    const url = `https://backend.housingbayarea.mtc.ca.gov/listings`
+    if (__ENV.DOORWAY_ENVIRONMENT !== undefined) {
+        const environment = __ENV.DOORWAY_ENVIRONMENT
+        url = `https://backend.${environment}.housingbayarea.mtc.ca.gov/listings`
+    }
     get(url);
 }
