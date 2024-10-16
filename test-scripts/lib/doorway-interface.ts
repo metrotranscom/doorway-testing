01228231d3d986a7f13ce9f52c891b1226c9b46e
Glenn Brown <gbrown@bayareametro.gov>
2024-10-16 14:14:11 -0700

    rearranging code and limiting artillery memory footprint

---
+++ test-scripts/lib/doorway-interface.ts
@@ -0,0 +1,154 @@
+import axios from "axios";
+import * as fs from "fs";
+import { DoorwayLogin } from "./doorway-login";
+import { instance } from "./winston.logger";
+export class DoorwayInterface {
+  url: string;
+  user: string;
+  password: string;
+  logger: typeof instance;
+  doorwayLogin: DoorwayLogin;
+  constructor(user: string, password: string, url: string) {
+    this.url = url;
+    this.user = user;
+    this.password = password;
+    this.logger = instance;
+    this.doorwayLogin = new DoorwayLogin(user, password, url);
+  }
+  async get<T = any>(endpoint: string, query?: object): Promise<T> {
+    this.logger.info("Logging into Doorway");
+    let data: T;
+    try {
+      const tokens = await this.doorwayLogin.login();
+      const getURL = `${this.url}${endpoint}`;
+      axios.defaults.withCredentials = true;
+      try {
+        const response = await axios.get(getURL, {
+          headers: {
+            Cookie: `${tokens["accessToken"]};${tokens["refreshToken"]}`,
+          },
+          params: query != undefined ? query : "",
+        });
+        if (response.data.items != undefined) {
+          data = response.data.items;
+        } else {
+          data = response.data;
+        }
+      } catch (getError) {
+        if (axios.isAxiosError(getError)) {
+          this.logger.error(
+            `Get from ${getURL} failed with an error code of ${getError.code}`,
+          );
+          this.logger.error(getError.message);
+          throw getError;
+        }
+      }
+    } catch (error) {
+      this.logger.error("Problems logging into Doorway!");
+      this.logger.error(error);
+      throw error;
+    }
+    return data!;
+  }
+  async delete(endpoint: string, query?: object): Promise<void> {
+    this.logger.info("Logging into Doorway");
+    try {
+      const tokens = await this.doorwayLogin.login();
+      const getURL = `${this.url}${endpoint}`;
+      axios.defaults.withCredentials = true;
+      try {
+        const response = await axios.delete(getURL, {
+          headers: {
+            Cookie: `${tokens["accessToken"]};${tokens["refreshToken"]}`,
+          },
+          data: query != undefined ? query : "",
+        });
+      } catch (getError) {
+        if (axios.isAxiosError(getError)) {
+          this.logger.error(
+            `Get from ${getURL} failed with an error code of ${getError.code}`,
+          );
+          this.logger.error(getError.message);
+          throw getError;
+        }
+      }
+    } catch (error) {
+      this.logger.error("Problems logging into Doorway!");
+      this.logger.error(error);
+      throw error;
+    }
+  }
+  async binaryGet(
+    endpoint: string,
+    query?: object,
+    filename = "/tmp/lottery.zip",
+  ): Promise<void> {
+    this.logger.info("Logging into Doorway");
+    try {
+      const tokens = await this.doorwayLogin.login();
+      const getURL = `${this.url}${endpoint}`;
+      axios.defaults.withCredentials = true;
+      try {
+        const writer = fs.createWriteStream(filename);
+        const response = await axios
+          .get(getURL, {
+            responseType: "stream",
+            headers: {
+              Cookie: `${tokens["accessToken"]};${tokens["refreshToken"]}`,
+            },
+            params: query != undefined ? query : "",
+          })
+          .then((response) => {
+            response.data.pipe(writer);
+          });
+      } catch (getError) {
+        if (axios.isAxiosError(getError)) {
+          this.logger.error(
+            `Get from ${getURL} failed with an error code of ${getError.code}`,
+          );
+          this.logger.error(getError.message);
+          throw getError;
+        }
+      }
+    } catch (error) {
+      this.logger.error("Problems logging into Doorway!");
+      this.logger.error(error);
+      throw error;
+    }
+  }
+  async post<T = any>(endpoint: string, body: T): Promise<T> {
+    this.logger.info("Logging into Doorway");
+    let data: T;
+    try {
+      const tokens = await this.doorwayLogin.login();
+      const getURL = `${this.url}${endpoint}`;
+      axios.defaults.withCredentials = true;
+      try {
+        const response = await axios.post(getURL, {
+          headers: {
+            Cookie: `${tokens["accessToken"]};${tokens["refreshToken"]}`,
+          },
+          body: body,
+        });
+        if (response.data.items != undefined) {
+          data = response.data.items;
+        } else {
+          data = response.data;
+        }
+      } catch (getError) {
+        if (axios.isAxiosError(getError)) {
+          this.logger.error(
+            `Get from ${getURL} failed with an error code of ${getError.code}`,
+          );
+          this.logger.error(getError.message);
+          throw getError;
+        }
+      }
+    } catch (error) {
+      this.logger.error("Problems logging into Doorway!");
+      this.logger.error(error);
+      throw error;
+    }
+    return data!;
+  }
+}
---
+++ test-scripts/lib/listings.ts
@@ -0,0 +1,65 @@
+import Fuse from "fuse.js";
+import { Listing } from "./apioutput";
+import { MatchingError } from "./custom-errors";
+import { DoorwayInterface } from "./doorway-interface";
+import { instance } from "./winston.logger";
+export class ListingsInterface {
+  constructor(
+    user: string,
+    password: string,
+    url: string,
+    passkey: string,
+    maxListings: number = 10000,
+  ) {
+    this.url = url;
+    this.logger = instance;
+    this.maxListings = maxListings;
+    this.passkey = passkey;
+    this.doorwayInterface = new DoorwayInterface(user, password, url);
+  }
+  doorwayInterface: DoorwayInterface;
+  url: string;
+  passkey: string;
+  maxListings: number;
+  logger: typeof instance;
+  getUrl(): string {
+    return this.url;
+  }
+  async getAllListings(): Promise<Listing[]> {
+    this.logger.debug("Calling listings API");
+    const listings: Listing[] = await this.doorwayInterface.get("/listings");
+    this.logger.debug("Here are the listings: ");
+    await this.logger.debug(listings);
+    return listings;
+  }
+  async listingMatcher(
+    match: string,
+    threshold: number = 0.7,
+  ): Promise<Listing> {
+    this.logger.debug("Getting all the listings.");
+    const listings = await this.getAllListings();
+    await this.logger.debug(`${listings.length} total listings found`);
+    const fuse = new Fuse(listings, {
+      keys: ["name", "listingsBuildingAddress.street"],
+      isCaseSensitive: false,
+      includeScore: true,
+      threshold: threshold,
+    });
+    let result = await fuse.search(match);
+    this.logger.info(`${result.length} matches found`);
+    this.logger.debug(result[0].score);
+    if (result.length == 0) {
+      throw new MatchingError({
+        name: "NO_MATCH_FOR_LISTING",
+        message: `Cant Find Listing ${match}`,
+        cause: match,
+      });
+    }
+    return result[0].item;
+  }
+  async getListing(id: string): Promise<Listing> {
+    this.logger.info(`Getting Listing ${id}`);
+    const listing = await this.doorwayInterface.get<Listing>(`/listings/${id}`);
+    return listing;
+  }
+}
