484442cc729b1b5da5682bc24d7a626055cb433c
Glenn Brown <gbrown@bayareametro.gov>
2024-10-17 20:21:27 -0700

    fix: missed changing a variable name

--- artillery/lib/fake-app-for-artillery.ts
+++ artillery/lib/fake-app-for-artillery.ts
@@ -2,7 +2,7 @@ import { Listing } from "./apioutput";
 import { FakeApplication } from "./fake-application";
 import { ListingsInterface } from "./listings";
 async function generateApplication(req, context, events, next) {
-  const user = context.vars["user"];
+  const user = context.vars["email"];
   const password = context.vars["password"];
   const url = context.vars["url"];
   const listingId = context.vars["listingId"];
