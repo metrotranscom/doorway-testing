2dcf476ba8e941a2e9be53b4366347dc9c5bd804
Glenn Brown <gbrown@bayareametro.gov>
2025-03-07 14:03:22 -0800

    fix: changes to get tests working and run for an hour

--- artillery/lib/fake-app-for-artillery.ts
+++ artillery/lib/fake-app-for-artillery.ts
@@ -1,16 +1,15 @@
-import { Listing } from "./apioutput";
 import { FakeApplication } from "./fake-application";
 import { ListingsInterface } from "./listings";
+import { instance as logger } from "./winston.logger";
 async function generateApplication(req, context, events, next) {
   const user = context.vars["email"];
   const password = context.vars["password"];
   const url = context.vars["url"];
+  logger.info(`Generating application for ${user} on ${url} with password ${password}`);
   const listingId = context.vars["listingId"];
   const listings = new ListingsInterface(user, password, url, "passkey");
-  const listing: Listing = await listings.getListing(listingId);
   const fakeApp = new FakeApplication(
-    listingId,
-    listing.listingMultiselectQuestions!,
+    listingId,[]
   );
   req.json = fakeApp;
 }
